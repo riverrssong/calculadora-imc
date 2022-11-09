441c5f47f8274ac54e139c929f359166fad7df47
riverrssong <110188188+riverrssong@users.noreply.github.com>
2022-11-08 22:59:14 -0300

    main

--- script/index.js
+++ script/index.js
@@ -3,12 +3,21 @@ function calcularIMC() {
     let altura = parseFloat(document.getElementById("altura").value);
     let peso = parseFloat(document.getElementById("peso").value);
     let imc = peso/(altura * altura);
-    if(imc > 18.5) {
-    mensagens.innerText="Baixo Peso";
-    } else if (imc >=18.5 && imc <=24.9){
-    mensagens.innerText="Peso normal";
-    } else if (imc >=25 && imc <=29.9){
-    mensagens.innerText="Peso normal";   
-    }
-    mensagens.innerText="Peso normal";
+    
+    if (imc < 18.5) {
+        mensagens.innerText="Baixo Peso";
+        } else if (imc < 24.9){
+        mensagens.innerText="Peso normal";
+        } else if (imc < 29.9){             
+        mensagens.innerText="Sobrepeso";   
+        } else if (imc < 39.9){
+        mensagens.innerText="Obesidade";          
+        } else if (imc > 39.9){
+        mensagens.innerText="Obesidade mórbida";
+        }
+}
+
+function redefinir () {
+    let apagar = document.getElementById("apagar");
+    altura.value = {}
 }
